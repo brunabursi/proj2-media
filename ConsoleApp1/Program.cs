29b448f877b7910c19d140829f271a7b2de7137e
Bruna Bursi dos Santos <bruna.bursi@verity.com.br>
2018-11-08 16:17:20 -0200

    projeto de adição e média de n numeros

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, soma, cont;
+            double media;
+
+            soma = 0;
+            cont = 0;
+            idade = int.Parse(Console.ReadLine());
+
+            while (idade >= 0)
+            {
+                soma = soma + idade;
+                cont++;
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            media = soma / cont;
+
+            Console.WriteLine(media.ToString("F2"));
+            Console.ReadLine();
+
+        }
+    }
+}
